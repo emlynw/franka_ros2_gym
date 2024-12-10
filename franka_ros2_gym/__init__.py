d8bf261126a76af2d6c24d3799398249aba56d2c
emlynw <emlynw98@gmail.com>
2024-12-10 18:02:59 +0000

    strawb env, different ee orientation

--- franka_ros2_gym/__init__.py
+++ franka_ros2_gym/__init__.py
@@ -1,3 +1,4 @@
 from gymnasium.envs.registration import register
 
 register( id="franka_ros2_gym/ReachIKDeltaRealEnv-v0", entry_point="franka_ros2_gym.envs:ReachIKDeltaRealEnv" , max_episode_steps=1000)
+register( id="franka_ros2_gym/ReachIKDeltaRealStrawbEnv-v0", entry_point="franka_ros2_gym.envs:ReachIKDeltaRealStrawbEnv" , max_episode_steps=1000)
--- franka_ros2_gym/envs/__init__.py
+++ franka_ros2_gym/envs/__init__.py
@@ -1,1 +1,2 @@
 from franka_ros2_gym.envs.reach_ik_delta_real import ReachIKDeltaRealEnv
+from franka_ros2_gym.envs.reach_ik_delta_real_strawb import ReachIKDeltaRealStrawbEnv
