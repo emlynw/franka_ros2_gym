764a580b16b995a07fffc0a2159d8783d7522fee
emlynw <emlynw98@gmail.com>
2025-02-15 19:42:06 +0000

    sim based on this

--- franka_ros2_gym/test/calibration_real.py
+++ franka_ros2_gym/test/calibration_real.py
@@ -4,12 +4,15 @@ import cv2
 from franka_ros2_gym import envs
 import numpy as np
 import time
+from franka_env.envs.wrappers import Quat2EulerWrapper
+
 
 np.set_printoptions(suppress=True)
 
 def main():
     render_mode = "rgb_array"
     env = gym.make("franka_ros2_gym/ReachIKDeltaRealStrawbEnv", render_mode=render_mode, ee_dof=6, pos_scale= 0.01, rot_scale = 0.2)
+    # env = Quat2EulerWrapper(env)
     env = TimeLimit(env, max_episode_steps=50)    
     waitkey = 1
 
@@ -20,11 +23,12 @@ def main():
         action = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0])
         obs, reward, terminated, truncated, info = env.step(action)
         print(obs['state']['tcp_pose'])
-        if render_mode == "rgb_array":
-                pixels = obs["images"]["wrist1"]
-                cv2.imshow("pixels", cv2.resize(cv2.cvtColor(pixels, cv2.COLOR_RGB2BGR), (720, 720)))
-                cv2.waitKey(waitkey)
+        # if render_mode == "rgb_array":
+        #         pixels = obs["images"]["wrist1"]
+        #         cv2.imshow("pixels", cv2.resize(cv2.cvtColor(pixels, cv2.COLOR_RGB2BGR), (720, 720)))
+        #         cv2.waitKey(waitkey)
     final_pose = obs['state']['tcp_pose']
+    print(f"initial pose: {initial_pose}")
     print(f"final pose: {final_pose}")
     print(f"diff: {final_pose-initial_pose}")
         
